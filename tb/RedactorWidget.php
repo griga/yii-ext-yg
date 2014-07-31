96424936a8832f26c44b12b262b4efac891d02ec
Yura Griga <grigach@gmail.com>
2014-07-31 14:26:19 +0300

    new array syntax

--- tb/RedactorWidget.php
+++ tb/RedactorWidget.php
@@ -22,22 +22,22 @@ class RedactorWidget extends \CInputWidget
         ]);
         foreach (\Lang::getLanguages() as $lang => $langTitle) {
 
-            $out .= \CHtml::openTag('div', array(
+            $out .= \CHtml::openTag('div', [
                 'data-ml-language' => $langTitle,
                 'class' => $lang == \Lang::get() ? '' : 'hidden',
-            ));
-            $out .= \Yii::app()->controller->widget('ext.redactor.ImperaviRedactorWidget', array(
+            ]);
+            $out .= \Yii::app()->controller->widget('ext.redactor.ImperaviRedactorWidget', [
                 'model' => $this->model,
                 'attribute' => $this->attribute . '_' . $lang,
-                'options' => array(
+                'options' => [
                     'lang' => \Lang::get() == 'uk' ? 'ua' : \Lang::get(),
                     'iframe' => true,
                     'css' => '/themes/ekma/front/css/style.css',
                     'minHeight' => $this->height,
                     'imageUpload' => '/admin/dashboard/imageUpload',
                     'imageGetJson' => '/admin/dashboard/imageList',
-                ),
-            ),true );
+                ],
+            ],true );
             $out .= \CHtml::closeTag('div');
 
         }
